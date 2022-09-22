07b7ac071336167f7d345928630d13b821ea39e3
prottoy <86410549+palolik@users.noreply.github.com>
2022-09-22 23:23:09 +0600

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:crud/pages/add_student_page.dart';
+import 'package:crud/pages/list_student_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Flutter FireStore CRUD'),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AddStudentPage(),
+                  ),
+                )
+              },
+              child: const Text('Add', style: TextStyle(fontSize: 20.0)),
+              style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+            )
+          ],
+        ),
+      ),
+      body: const ListStudentPage(),
+    );
+  }
+}
